f9f596d61cb78273dc60671dfae2171a4c62759d
Luis Carta <luis.carta@nokia.com>
2021-03-29 22:42:57 +0100

    Initial import

---
+++ src/main/java/com/carta/nbaservice/NbaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carta.nbaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaServiceApplication.class, args);
+    }
+
+}
